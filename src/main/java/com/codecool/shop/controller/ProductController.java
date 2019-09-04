9fe7a98359f708e73a2693a758331639ceac4a40
Mark Ungvari <ungvarimark@gmail.com>
2019-09-05 01:29:24 +0200

    Merge branch 'dev' of /home/hunnyboy/Code_Cool/OOP/webshop-java-wizardsense with conflicts.

--- src/main/java/com/codecool/shop/controller/ProductController.java
+++ src/main/java/com/codecool/shop/controller/ProductController.java
@@ -1,12 +1,15 @@
 package com.codecool.shop.controller;
 
+import com.codecool.shop.dao.CartDao;
 import com.codecool.shop.dao.ProductCategoryDao;
 import com.codecool.shop.dao.ProductDao;
 import com.codecool.shop.dao.SupplierDao;
+import com.codecool.shop.dao.implementation.CartDaoMem;
 import com.codecool.shop.dao.implementation.ProductCategoryDaoMem;
 import com.codecool.shop.dao.implementation.ProductDaoMem;
 import com.codecool.shop.config.TemplateEngineUtil;
 import com.codecool.shop.dao.implementation.SupplierDaoMem;
+import com.codecool.shop.model.Product;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.WebContext;
 
@@ -16,29 +19,71 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Collections;
+import java.util.Enumeration;
+import java.util.List;
+import java.util.stream.Collectors;
+
 
 @WebServlet(urlPatterns = {"/"})
 public class ProductController extends HttpServlet {
+
+    private Object filter(ProductCategoryDao pCD, ProductDao pDS, HttpServletRequest req) {
+        List<String> headers = Collections.list(req.getParameterNames());
+
+        if (headers.contains("filter")) {
+            List<Character> filtered = req.getParameter("filter").chars().mapToObj(e -> (char) e).collect(Collectors.toList());
+            int filterId = Integer.parseInt(filtered.get(filtered.size() -1).toString());
+            filtered.remove(filtered.size() - 1);
+            String filterName = filtered.toString().trim();
+
+            String suppliers = SupplierDaoMem.getInstance().getAll().toString();
+
+            if (suppliers.contains(filterName)) {
+                return pDS.getBy(SupplierDaoMem.getInstance().find(filterId));
+            } else {
+                return pDS.getBy(pCD.find(filterId));
+            }
+        } else {
+            return pDS.getAll();
+        }
+    }
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         ProductDao productDataStore = ProductDaoMem.getInstance();
         ProductCategoryDao productCategoryDataStore = ProductCategoryDaoMem.getInstance();
+        SupplierDao supplierDataStore = SupplierDaoMem.getInstance();
 
         TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(req.getServletContext());
         WebContext context = new WebContext(req, resp, req.getServletContext());
 
+
+        Object filterBy = filter(productCategoryDataStore, productDataStore, req);
         context.setVariable("categories" , productCategoryDataStore.getAll());
-        context.setVariable("suppliers" , SupplierDaoMem.getInstance().getAll());
-        context.setVariable("products", productDataStore.getAll());
-        // // Alternative setting of the template context
-        // Map<String, Object> params = new HashMap<>();
-        // params.put("category", productCategoryDataStore.find(1));
-        // params.put("products", productDataStore.getBy(productCategoryDataStore.find(1)));
-        // context.setVariables(params);
+        context.setVariable("products", filterBy);
+
+
         engine.process("product/index.html", context, resp.getWriter());
     }
 
+    @Override
+    protected void doPost (HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        CartDao cartDaoStore = CartDaoMem.getInstance();
+        ProductDao productDataStore = ProductDaoMem.getInstance();
+
+        String productId = req.getParameter("product");
+        cartDaoStore.add(productDataStore.find(Integer.parseInt(productId)));
+
+        doGet(req, resp);
+    }
+
+
+
 }
+
+// // Alternative setting of the template context
+// Map<String, Object> params = new HashMap<>();
+// params.put("category", productCategoryDataStore.find(1));
+// params.put("products", productDataStore.getBy(productCategoryDataStore.find(1)));
+// context.setVariables(params);
