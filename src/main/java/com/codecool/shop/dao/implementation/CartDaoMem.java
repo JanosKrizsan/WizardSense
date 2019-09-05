383b37621cc1d934c53c8f9c79251185fc8ca1f1
Mark Ungvari <ungvarimark@gmail.com>
2019-09-05 10:00:15 +0200

    Readded the files and methods after a mishap during the night.

---
+++ src/main/java/com/codecool/shop/dao/implementation/CartDaoMem.java
@@ -0,0 +1,49 @@
+package com.codecool.shop.dao.implementation;
+
+import com.codecool.shop.dao.CartDao;
+import com.codecool.shop.dao.SupplierDao;
+import com.codecool.shop.model.Product;
+import com.codecool.shop.model.Supplier;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CartDaoMem implements CartDao {
+
+    private List<Product> data = new ArrayList<>();
+    private static CartDaoMem instance = null;
+
+    /* A private Constructor prevents any other class from instantiating.
+     */
+    private CartDaoMem() {
+    }
+
+    public static CartDaoMem getInstance() {
+        if (instance == null) {
+            instance = new CartDaoMem();
+        }
+        return instance;
+    }
+
+    @Override
+    public void add(Product product) {
+        product.setId(data.size() + 1);
+        data.add(product);
+    }
+
+    @Override
+    public Product find(int id) {
+        return data.get(id);
+    }
+
+    @Override
+    public void remove(int id) {
+        data.remove(id);
+    }
+
+    @Override
+    public List<Product> getAll() {
+        return data;
+    }
+
+}
