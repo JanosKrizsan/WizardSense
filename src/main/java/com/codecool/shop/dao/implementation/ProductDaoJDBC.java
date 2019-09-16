82c822bf0481236034fba7aa1ca06c4370c4fc33
JanosKrizsan <janoskrizsan402@gmail.com>
2019-09-16 18:59:02 +0200

    cleaned up imports

--- src/main/java/com/codecool/shop/dao/implementation/ProductDaoJDBC.java
+++ src/main/java/com/codecool/shop/dao/implementation/ProductDaoJDBC.java
@@ -9,9 +9,7 @@ import com.codecool.shop.model.Supplier;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.ArrayList;
-import java.util.Currency;
 import java.util.List;
 import java.util.stream.Collectors;
 
